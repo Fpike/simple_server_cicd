0e842bbc8a55bdecef9f811144758ffbbdb4f2ab
Frankie <frankiepike@live.co.uk>
2024-10-31 15:22:09 +0000

    improve my message

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_home():
-    return "Hello, Frankie!"
+    return "Hello, Frankie, what's uuuup!"
 
 if __name__ == '__main__':
     app.run(
